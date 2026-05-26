19ec588f2ab675c59dee9d8d72cd196df9db6ac8
boma026 <arthurboma@yahoo.com.br>
2026-05-26 07:49:18 -0300

    feat: category form

--- src/app/(protected)/category/create/page.tsx
+++ src/app/(protected)/category/create/page.tsx
@@ -3,46 +3,57 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { Category } from "@/types/Category";
 import { api } from "@/utils/axios";
-import { Delete, SquarePen } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function CategoryPage() {
-  const [categories, setCategories] = useState<Category[]>([]);
+  const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
+  const { register, handleSubmit } = useForm<Category>();
 
-  useEffect(() => {
-    const fetchCategory = async () => {
+  /*const handleCreateCategory = async () => {
       try {
-        const res = await api.get("/category");
+        const res = await api.post("/category", {
+          body: 
+        });
         console.log("category", res.data);
-        setCategories(res.data);
       } catch (error: unknown) {
         console.error("Erro na requisição:", error);
       } finally {
         setLoading(false);
+        router.push("/category");
       }
     };
-
-    fetchCategory();
-  }, []);
-
+*/
   return (
     <div className="p-4 flex flex-col w-full min-h-screen gap-2">
       <header className="flex justify-between ">
         <p className="title">Nova categoria</p>
       </header>
       <hr />
-      <Input />
+      <form>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="category" className="font-extrabold">
+            Nome da categoria
+          </label>
+          <Input
+            className="w-1/2"
+            placeholder="Nome da categoria"
+            id="category"
+            type="text"
+            {...register("name", { required: true })}
+          />
+          <Button size="lg" className="w-1/12" type="submit">
+            Criar categoria
+          </Button>
+        </div>
+      </form>
+      <div className="fixed bottom-4 right-4">
+        <ModeToggle />
+      </div>
     </div>
   );
 }
